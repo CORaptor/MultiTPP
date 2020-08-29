ecf85e5448423f422cc818aa9c5052b8b5a1dcfe
Orthotomus <covelociraptor@gmail.com>
2020-08-29 10:29:09 +0200

    initial commit

---
+++ Source/MultiTPP/MultiTPP.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MultiTPP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiTPP, "MultiTPP" );
+ 
---
+++ Source/MultiTPP/MultiTPPGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiTPPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMultiTPPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMultiTPPGameMode();
+};
+
+
+
